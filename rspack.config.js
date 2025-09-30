1e982d827f0e6f41a64df314d2c9ff21c635b3a8
Shrey719 <shrey150436@gmail.com>
2025-09-30 14:12:48 -0400

    bones

---
+++ rspack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+
+const clientConfig = {
+  devtool: false, 
+  name: "client",
+  target: "web",
+  resolve: {
+    extensions: ['.js', '.mjs', '.jsx', '.ts', '.tsx', '.json'], 
+  },
+  entry: {
+    functions: "./src/client/main.ts",
+  },
+  output: {
+    filename: "latens.client.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'esbuild-loader',
+        options: {
+          loader: 'ts',
+          target: 'es2020',
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
+
+const swConfig = {
+  devtool: false, 
+  resolve: {
+    extensions: ['.js', '.mjs', '.jsx', '.ts', '.tsx', '.json'], 
+  },
+  name: "sw",
+  target: "webworker",
+  entry: {
+    sw: "./src/serviceWorker/main.ts",
+  },
+  output: {
+    filename: "latens.sw.bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'esbuild-loader',
+        options: {
+          loader: 'ts',
+          target: 'es2020',
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
+
+module.exports = [clientConfig, swConfig];
